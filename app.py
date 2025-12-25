f018770cf8c7aa9bb822047ac5ec7cf924e9a7f3
LeonR92 <127194165+LeonR92@users.noreply.github.com>
2025-12-26 00:28:08 +0100

    feature(sysdesign): add sys design agent (#6)
    Add a system design agent to design a robust system with a list of tech stack and pricing

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import asyncio
 from dependency import Dependency
 from pricing_agent.agent import pricing_agent
 from requirement_agent.agent import requirement_agent
+from sysdesign_agent.agent import sysdesign_agent
 from user_prompt import USER_PROMPT
 
 
@@ -10,8 +11,15 @@ async def main(user_prompt: str):
 
     req_result = await requirement_agent.run(user_prompt)
     tech_list = req_result.output.suggested_technologies
+    design_prompt = (
+        f"Design a system architecture using these technologies: {', '.join(tech_list)}"
+    )
+    design_result = await sysdesign_agent.run(design_prompt)
 
-    pricing_prompt = f"Please provide a price breakdown for these technologies: {', '.join(tech_list)}"
+    components_str = ", ".join([c.name for c in design_result.output.components])
+    pricing_prompt = (
+        f"Provide a price breakdown for this architecture: {components_str}"
+    )
     my_deps = Dependency(api_key="sk-your-fake-key-here")
     final_result = await pricing_agent.run(pricing_prompt, deps=my_deps)
 
---
+++ sysdesign_agent/prompt.py
@@ -0,0 +1,2 @@
+SYSTEM_DESIGN_AGENT_PROMPT = """You are System design agent, an AI specialized in system design and architecture. Your role is to assist users in creating efficient, scalable,
+ and maintainable system designs based on their a list of tools and pricing."""
